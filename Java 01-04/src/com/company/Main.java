6e88662c93be2b5814cfcea1f6df5b86897ee081
patrykhubar <patryk.hubar1@gmail.com>
2020-11-07 19:51:05 +0100

    Added some solutions

--- Java 01-04/src/com/company/Main.java
+++ Java 01-04/src/com/company/Main.java
@@ -1,8 +1,175 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Zajęcia 01-04");
+
+
+        System.out.println("Zajęcia 01-03");
+
+
+        //--------------------------------------------------------------------
+
+
+
+
+
+
+        System.out.println("Zajęcia przesunięcia binarne zajęcia dodatkowe");
+
+
+        int j = 13;
+        byte k = (byte) j;
+
+        System.out.println(k);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //---------------------------------------------------------------
+
+        System.out.println("Zajęcia 04 ------------------");
+
+
+
+
+
+
+        //1
+        System.out.println("Zadanie 1");
+
+        int liczba = 0b101010;
+        int x = 0b100001;
+        System.out.print("Na bicie 0: ");
+        System.out.println(Integer.toBinaryString(liczba & x));
+
+
+
+
+
+        //2
+        System.out.println("Zadanie 3");
+
+
+
+
+        //3
+        System.out.println("Zadanie 3");
+
+        int zmienna = 666;
+        System.out.println(zmienna);
+
+
+
+        //4
+        System.out.println("Zadanie 4");
+
+        int p123 = 123;
+
+        // int 321 = 321;
+
+        // int @asf = 123;
+
+        // public = 123;
+
+        // null = 123;
+
+
+
+        //5
+        System.out.println("Zadanie 5");
+
+        char charValue = 'a';
+        char charvalue = 'z';
+
+        System.out.println(charvalue - charValue);
+
+
+
+        //6
+        System.out.println("Zadanie 6");
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println((bool1 && ! bool2) || (bool2 && ! bool1));
+
+
+
+        //7
+        System.out.println("Zadanie 7");
+
+
+        int y = 2 * 5 + 3 * 4 - 8;
+
+        // 10 + 12 - 8
+        System.out.println(x);
+
+
+
+        //8
+        System.out.println("Zadanie 8");
+
+        // java.util.Scanner in = new java.util.Scanner(System.in);
+        // int studentId = in.nextInt();
+
+        // System.out.println("Hello " + "s" + studentId);
+
+
+
+
+        //9
+        System.out.println("Zadanie 9");
+
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a / b + " reszty " + a % b);
+
+
+
+        //10
+        System.out.println("Zadanie 10");
+
+        int i ;
+        boolean czyPada = false;
+
+        if (czyPada == true){
+
+            i = 5;
+
+        }else{
+
+            i = 8;
+        }
+
+        System.out.println(i);
+
+
+
+        //11
+        System.out.println("Zadanie 11");
+
+        boolean czyPada1 = true;
+        int czySwieciSlonce;
+
+
+
+
+
+
+
     }
 }
--- Java 05/src/Main.java
+++ Java 05/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zajęcia 05");
-        System.out.println("test");
+
     }
 }
